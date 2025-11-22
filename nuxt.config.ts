cd9be043ac00882437b40200b2e5c090bb1163bf
Matthew Gilmore <69724634+mattsg6@users.noreply.github.com>
2025-11-22 16:37:48 -0500

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
+   nitro: {
+    preset: 'github-pages'
+  },
+  app: {
+    baseURL: '/',
+    buildAssetsDir: 'assets'
+  },
+  routeRules: {
+    '/**': { prerender: true }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
   ssr: false,
