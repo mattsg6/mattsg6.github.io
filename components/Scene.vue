17bd29898d425dba5edf7c7af48b11572273832c
Matthew Gilmore <69724634+mattsg6@users.noreply.github.com>
2025-11-23 22:45:44 -0500

    moving person

--- components/Scene.vue
+++ components/Scene.vue
@@ -10,44 +10,141 @@ onMounted(() => {
   renderer.setClearColor(0xffffff, 1);
   renderer.setPixelRatio(window.devicePixelRatio);
 
-  let width = window.innerWidth;
-  let height = window.innerHeight;
+  let width = canvas.clientWidth;
+  let height = canvas.clientHeight;
 
-  const groundLevel = 250;
+  const startPosition = -20;
+  const groundLevel = 0;
+  const torsoHeight = 160;
+  const walkSpeed = 0.75;
+  const armSpeed = {
+    t: 0,
+    freq: 1,
+    amount: 25,
+  };
 
   const camera = new THREE.OrthographicCamera(0, width, height, 0, -10, 10);
+  camera.aspect = width / height;
   camera.position.z = 1;
 
   const scene = new THREE.Scene();
 
-  // Sun
-  const geometry = new THREE.TorusGeometry(50, 1, 16, 100); // radius, thickness
   const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
-  const sun = new THREE.Mesh(geometry, material);
-  sun.position.set(200, window.innerHeight - 150, 0);
+
+  // Sun
+  const sunGeometry = new THREE.TorusGeometry(60, 0.5, 16, 100); // radius, thickness
+  const sun = new THREE.Mesh(sunGeometry, material);
+  sun.position.set(200, height - 150, 0);
   scene.add(sun);
 
-  // Ground
-  const groundPoints = [];
-  groundPoints.push(new THREE.Vector3(0, groundLevel, 0));
-  groundPoints.push(new THREE.Vector3(width, groundLevel, 0));
-  const groundGeometry = new THREE.BufferGeometry().setFromPoints(groundPoints);
-  const groundLine = new THREE.Line(groundGeometry, material)
-  scene.add(groundLine);
+  // Man
+  // Torso
+  const torsoPoints = [];
+  torsoPoints.push(new THREE.Vector3(startPosition, groundLevel + 75, 0));
+  torsoPoints.push(
+    new THREE.Vector3(startPosition, groundLevel + torsoHeight, 0)
+  );
+  const torsoGeometry = new THREE.BufferGeometry().setFromPoints(torsoPoints);
+  const torsoLine = new THREE.Line(torsoGeometry, material);
+  scene.add(torsoLine);
+  // head
+  const headGeometry = new THREE.TorusGeometry(20, 0.5, 16, 100);
+  const head = new THREE.Mesh(headGeometry, material);
+  head.position.set(startPosition, groundLevel + torsoHeight + 20, 0);
+  scene.add(head);
+  // arms
+  const armPoints = [];
+  armPoints.push(
+    new THREE.Vector3(
+      startPosition - armSpeed.amount,
+      groundLevel + torsoHeight - 50,
+      0
+    )
+  );
+  armPoints.push(
+    new THREE.Vector3(startPosition, groundLevel + torsoHeight - 25, 0)
+  );
+  armPoints.push(
+    new THREE.Vector3(
+      startPosition + armSpeed.amount,
+      groundLevel + torsoHeight - 50,
+      0
+    )
+  );
+  const armGeometry = new THREE.BufferGeometry().setFromPoints(armPoints);
+  const armLine = new THREE.Line(armGeometry, material);
+  scene.add(armLine);
+  // legs
 
-  function onWindowResize() {
-    width = window.innerWidth;
-    height = window.innerHeight;
-
-    renderer.setSize(width, height);
-
-    camera.left = 0;
-    camera.right = width;
-    camera.top = height;
-    camera.bottom = 0;
-    camera.updateProjectionMatrix();
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const nWidth = canvas.clientWidth;
+    const nHeight = canvas.clientHeight;
+    const needResize = nWidth !== canvas.width || nHeight !== canvas.height;
+    if (needResize) {
+      renderer.setSize(nWidth, nHeight, false);
+      width = nWidth;
+      height = nHeight;
+    }
+    return needResize;
   }
-  window.addEventListener("resize", onWindowResize, false);
+
+  function resetPosition() {
+    const tPositions = torsoLine.geometry.attributes.position;
+    tPositions.setX(0, startPosition);
+    tPositions.setX(1, startPosition);
+    head.position.set(
+      startPosition,
+      groundLevel + torsoHeight + 20,
+      0
+    );
+    const aPositions = armLine.geometry.attributes.position;
+    aPositions.setX(1, startPosition);
+  }
+
+  function render() {
+    // torso
+    const tPositions = torsoLine.geometry.attributes.position;
+    if(tPositions.getX(0) - 30 > width) {
+      resetPosition();
+    }
+    tPositions.setX(0, tPositions.getX(0) + walkSpeed);
+    tPositions.setX(1, tPositions.getX(1) + walkSpeed);
+    tPositions.needsUpdate = true;
+
+    // head
+    head.position.set(
+      head.position.x + walkSpeed,
+      groundLevel + torsoHeight + 20,
+      0
+    );
+
+    // arms
+    armSpeed.t += walkSpeed * 0.05;
+    const swing = Math.sin(armSpeed.t * armSpeed.freq);
+    const aPositions = armLine.geometry.attributes.position;
+    aPositions.setX(0, aPositions.getX(1) + swing * armSpeed.amount);
+    aPositions.setX(1, aPositions.getX(1) + walkSpeed);
+    aPositions.setX(2, aPositions.getX(1) - swing * armSpeed.amount);
+    aPositions.needsUpdate = true;
+
+    // resize
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      const w = canvas.width;
+      const h = canvas.height;
+
+      camera.left = 0;
+      camera.right = w;
+      camera.top = h;
+      camera.bottom = 0;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 
   renderer.setSize(width, height);
   renderer.render(scene, camera);
@@ -55,8 +152,8 @@ onMounted(() => {
 </script>
 <style scoped>
 #c {
-  width: 100%;
-  height: 100%;
+  width: 100% !important;
+  height: 100% !important;
   display: block;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
-    <v-container fluid class="pa-0 ma-0 h-100">
-        <v-row class="w-100 d-flex justify-center pb-5 pt-10 ma-0">
+    <v-container fluid class="pa-0 ma-0 d-flex flex-column h-screen overflow-hidden">
+        <v-row class="w-100 d-flex justify-center pb-5 pt-10 ma-0" style="max-height: 6.25rem;">
             <span class="text-h3" style="text-align: center;">Matthew Gilmore</span>
         </v-row>
-        <v-row class="w-100 d-flex justify-center ga-6 flex-wrap ma-0">
+        <v-row class="w-100 d-flex justify-center ga-6 flex-wrap ma-0" style="max-height: 1.5rem;">
             <template v-for="item in menu">
                 <a target="_blank" :href="item.src">{{ item.name }}</a>
             </template>
         </v-row>
-        <v-row class="pa-0 ma-0 w-100 h-100">
+        <v-row class="pa-0 ma-0 w-100 flex-grow-1" style="min-height: 0; height: 100%;">
             <scene></scene>
         </v-row>
     </v-container>
