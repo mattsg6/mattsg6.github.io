fe58abd214561eded6f643232ddf5fea81c90377
Matthew Gilmore <69724634+mattsg6@users.noreply.github.com>
2025-11-24 23:27:40 -0500

    trying to get arms to swing

--- components/Scene.vue
+++ components/Scene.vue
@@ -3,9 +3,6 @@
 </template>
 <script setup>
 import * as THREE from "three";
-import { Line2 } from "three/examples/jsm/lines/Line2.js";
-import { LineMaterial } from "three/examples/jsm/lines/LineMaterial.js";
-import { LineGeometry } from "three/examples/jsm/lines/LineGeometry.js";
 
 onMounted(() => {
   const canvas = document.querySelector("#c");
@@ -17,15 +14,11 @@ onMounted(() => {
   let width = canvas.clientWidth;
   let height = canvas.clientHeight;
 
-  const startPosition = -20;
+  const startPosition = -20; // -20
   const groundLevel = 0;
   const torsoHeight = 160;
-  const walkSpeed = 0.75;
-  const armSpeed = {
-    t: 0,
-    freq: 1,
-    amount: 25,
-  };
+  const walkSpeed = 0.75 / pixelRatio;
+  let time = 0;
 
   const camera = new THREE.OrthographicCamera(0, width, height, 0, -10, 10);
   camera.aspect = width / height;
@@ -43,40 +36,34 @@ onMounted(() => {
 
   // Man
   // Torso
-  const torsoPoints = [
-    new THREE.Vector3(startPosition, groundLevel + 75, 0),
-    new THREE.Vector3(startPosition, groundLevel + torsoHeight, 0)
-  ];
-  const torsoGeometry = new THREE.BufferGeometry().setFromPoints(torsoPoints);
-  const torsoLine = new THREE.Line(torsoGeometry, material);
-  scene.add(torsoLine);
+  const torsoGeometry = new THREE.CylinderGeometry(1, 1, torsoHeight - 84);
+  const torso = new THREE.Mesh(torsoGeometry, material);
+  torso.position.set(startPosition, torsoHeight / 2 + 42, 0);
+  scene.add(torso);
   // head
   const headGeometry = new THREE.TorusGeometry(20, 1, 16, 100);
   const head = new THREE.Mesh(headGeometry, material);
   head.position.set(startPosition, groundLevel + torsoHeight + 20, 0);
   scene.add(head);
   // arms
-  const armPoints = [];
-  armPoints.push(
-    new THREE.Vector3(
-      startPosition - armSpeed.amount,
-      groundLevel + torsoHeight - 50,
-      0
-    )
+  const armLPivot = new THREE.Object3D();
+  armLPivot.position.set(startPosition - 13.5, torsoHeight - (torsoHeight/5), 0);
+  scene.add(armLPivot);
+  const armLMesh = new THREE.Mesh(
+    new THREE.CylinderGeometry(1, 1, torsoHeight / 4),
+    material
   );
-  armPoints.push(
-    new THREE.Vector3(startPosition, groundLevel + torsoHeight - 25, 0)
+  armLPivot.add(armLMesh);
+  armLPivot.rotation.z = -Math.PI / 4;
+  const armRPivot = new THREE.Object3D();
+  armRPivot.position.set(startPosition + 13.5, torsoHeight - (torsoHeight/5), 0);
+  scene.add(armRPivot);
+  const armRMesh = new THREE.Mesh(
+    new THREE.CylinderGeometry(1, 1, torsoHeight / 4),
+    material
   );
-  armPoints.push(
-    new THREE.Vector3(
-      startPosition + armSpeed.amount,
-      groundLevel + torsoHeight - 50,
-      0
-    )
-  );
-  const armGeometry = new THREE.BufferGeometry().setFromPoints(armPoints);
-  const armLine = new THREE.Line(armGeometry, material);
-  scene.add(armLine);
+  armRPivot.add(armRMesh);
+  armRPivot.rotation.z = Math.PI / 4;
   // legs
 
   function resizeRendererToDisplaySize(renderer) {
@@ -93,39 +80,38 @@ onMounted(() => {
   }
 
   function resetPosition() {
-    const tPositions = torsoLine.geometry.attributes.position;
-    tPositions.setX(0, startPosition);
-    tPositions.setX(1, startPosition);
+    torso.position.set(startPosition, torsoHeight / 2 + 42, 0);
     head.position.set(startPosition, groundLevel + torsoHeight + 20, 0);
-    const aPositions = armLine.geometry.attributes.position;
-    aPositions.setX(1, startPosition);
+    armLPivot.position.set(
+      startPosition - 13.5,
+      torsoHeight - torsoHeight / 5,
+      0
+    );
+    armRPivot.position.set(
+      startPosition + 13.5,
+      torsoHeight - torsoHeight / 5,
+      0
+    );
   }
 
   function render() {
+    time += 0.0125;
+
     // torso
-    const tPositions = torsoLine.geometry.attributes.position;
-    if (tPositions.getX(0) - 30 > width) {
+    if(torso.position.x - 30 > width) {
       resetPosition();
     }
-    tPositions.setX(0, tPositions.getX(0) + walkSpeed);
-    tPositions.setX(1, tPositions.getX(1) + walkSpeed);
-    tPositions.needsUpdate = true;
+    torso.position.set(torso.position.x + walkSpeed, torso.position.y, 0);
 
     // head
-    head.position.set(
-      head.position.x + walkSpeed,
-      groundLevel + torsoHeight + 20,
-      0
-    );
+    head.position.set(head.position.x + walkSpeed, head.position.y, 0);
 
     // arms
-    armSpeed.t += walkSpeed * 0.05;
-    const swing = Math.sin(armSpeed.t * armSpeed.freq);
-    const aPositions = armLine.geometry.attributes.position;
-    aPositions.setX(0, aPositions.getX(1) + swing * armSpeed.amount);
-    aPositions.setX(1, aPositions.getX(1) + walkSpeed);
-    aPositions.setX(2, aPositions.getX(1) - swing * armSpeed.amount);
-    aPositions.needsUpdate = true;
+    // const swingAngle = (Math.sin(time) * Math.PI) / 2;
+    // armLPivot.rotation.z = Math.PI / 4 + swingAngle;
+    // armRPivot.rotation.z = -Math.PI / 4 - swingAngle;
+    armLPivot.position.x += walkSpeed;
+    armRPivot.position.x += walkSpeed;
 
     // resize
     if (resizeRendererToDisplaySize(renderer)) {
@@ -138,8 +124,6 @@ onMounted(() => {
       camera.top = h;
       camera.bottom = 0;
       camera.updateProjectionMatrix();
-
-      lineMaterial.resolution.set(w, h);
     }
 
     renderer.render(scene, camera);
